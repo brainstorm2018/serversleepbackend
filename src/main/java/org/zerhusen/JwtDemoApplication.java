3d7288fb7eb5c70f973aa8841061e3bf46962eaf
arditsarja <arditsarja@gmail.com>
2018-05-01 17:11:32 +0200

    Created version 1.0

---
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -0,0 +1,13 @@
+package org.zerhusen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoApplication {
+
+    public static void main(String[] args) {
+        TypeOfServer.getOs();
+        SpringApplication.run(JwtDemoApplication.class, args);
+    }
+}
